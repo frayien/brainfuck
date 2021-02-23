78fb09b99951d5fb51fa8a4fa81dbd48e36adb4d
Adrien <garbani.adrien@yahoo.fr>
2021-02-23 23:35:09 +0100

    gitignore

--- output.cpp
+++
@@ -1,3 +0,0 @@
-#include <cstdio>
-#include <iostream>
-int main() {char buffer[30000] = {0};char *ptr = buffer;ptr++;--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);while(*ptr){++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);ptr++;*ptr=getchar();--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);--(*ptr);}++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);++(*ptr);while(*ptr){ptr--;}ptr++;while(*ptr){putchar(*ptr);ptr++;}return 0;}
