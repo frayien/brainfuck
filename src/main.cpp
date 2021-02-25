cd200e9265367f0862aeece70e58f37f89a57cf9
Adrien <garbani.adrien@yahoo.fr>
2021-02-26 00:02:32 +0100

    test xbf

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,66 @@
 
 struct Config
 {
-    std::string input = "echo.bf";
-    std::string output = "output.cpp";
+    std::string input = "test.xbf";
+    std::string cpp_tmp_file = "_tmp_.cpp";
+    std::string bf_tmp_file = "_tmp_.bf";
     std::string appName = "App.exe";
-    bool executeWhenFinished = false;
+    bool executeWhenFinished = true;
+    bool extendedBrainfuck = true;
 } config;
+
+void print(char const& chr, std::istream & in, std::ostream & out)
+{
+    if(in.get() == '*') 
+    {
+        int n = 1;
+        if(in.get() == '\'')
+        {
+            if(in.get() != '\\') in.unget();
+
+            n = in.get();
+
+            if(in.get() != '\'') std::cout << "syntax error : missing ' at " << in.tellg() << std::endl;
+        }
+        else
+        {
+            in.unget();
+            in >> n;
+        }
+
+        out << std::string(n, chr);
+    }
+    else
+    {
+        in.unget();
+        out << chr;
+    }
+}
+
+void transcompile_xbf_to_bf(std::istream & in, std::ostream & out)
+{
+    char chr = 0;
+    while(!in.eof() && chr != EOF) {
+        chr = in.get();
+        switch(chr)
+        {
+        case '#': while(!in.eof() && in.get() != '#'); break;
+
+        case '>':
+        case '<':
+        case '+':
+        case '-':
+        case '.':
+        case ',':
+        case '[':
+        case ']':
+            print(chr, in, out);
+            break;
+        
+        default: break;
+        }
+    }
+}
 
 void transcompile(std::istream & in, std::ostream & out)
 {
@@ -28,7 +83,6 @@ void transcompile(std::istream & in, std::ostream & out)
         case '+': out << "++(*ptr);"; break;
         case '-': out << "--(*ptr);"; break;
         case '.': out << "putchar(*ptr);"; break;
-        //case '?': out << "std::cout << (int) *ptr << ' ';"; break;
         case ',': out << "*ptr=getchar();"; break;
         case '[': out << "while(*ptr){"; break;
         case ']': out << "}"; break;
@@ -42,13 +96,13 @@ void transcompile(std::istream & in, std::ostream & out)
 
 int compile(std::istream & in) 
 {
-    std::ofstream ofs(config.output);
+    std::ofstream ofs(config.cpp_tmp_file);
 
     transcompile(in, ofs);
 
     ofs.close();
 
-    return std::system(("g++ " + config.output + " -o " + config.appName + " -O3 --std=c++17 -Wall").c_str());
+    return std::system(("g++ " + config.cpp_tmp_file + " -o " + config.appName + " -O3 --std=c++17 -Wall").c_str());
 }
 
 void loadConfig(int argc, char *argv[])
@@ -75,7 +129,7 @@ void loadConfig(int argc, char *argv[])
         {
             if(i < argc)
             {
-                config.output = argv[i++];
+                config.cpp_tmp_file = argv[i++];
             }
         }
         else if(cmd == "-exe")
@@ -86,6 +140,14 @@ void loadConfig(int argc, char *argv[])
         {
             config.executeWhenFinished = false;
         }
+        else if(cmd == "-ext")
+        {
+            config.extendedBrainfuck = true;
+        }
+        else if(cmd == "-noext")
+        {
+            config.extendedBrainfuck = false;
+        }
     }
 }
 
@@ -94,6 +156,24 @@ int main(int argc, char *argv[])
     loadConfig(argc, argv);
 
     std::ifstream in(config.input);
+
+    if(config.extendedBrainfuck)
+    {
+        if(in.get() == '#' && in.get() == '!' && in.get() == 'x' && in.get() == 'b' && in.get() == 'f' && in.get() == '#') 
+        {
+            std::ofstream out(config.bf_tmp_file);
+
+            transcompile_xbf_to_bf(in, out);
+
+            out.close();
+            in.close();
+            in.open(config.bf_tmp_file);
+        }
+        else
+        {
+            in.seekg(0);
+        }
+    }
 
     compile(in);
 
